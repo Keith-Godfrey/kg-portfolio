e01074cbdbd60661c90b5a5ba096628c0b410875
Keith Godfrey <keith.godfrey23@gmail.com>
2024-09-25 13:38:35 -0700

    put icons in the links and added some spacing between links.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,17 @@
-export default function Footer() {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
+export default function Footer() {
   return (
     <footer className="footer">
       <p>Connect with me:</p>
-      <a href="https://github.com/Keith-Godfrey">GitHub</a>
-      <a href="https://linkedin.com/in/Keith-Godfrey">LinkedIn</a>
+      <a href="https://github.com/Keith-Godfrey" target="_blank" rel="noopener noreferrer">
+      
+        <FontAwesomeIcon icon={faGithub} /> GitHub
+      </a>
+      <a href="https://linkedin.com/in/Keith-Godfrey" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+      </a>
     </footer>
   );
 }
-
